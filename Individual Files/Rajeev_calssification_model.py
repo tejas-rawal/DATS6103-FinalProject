12831f88c09444245e79c8e8413f8ec55be31aa7
Rajeev Koneru <Rajeev.Koneru123@gmail.com>
2022-12-10 23:36:21 -0500

    minor code update

--- Individual Files/Rajeev_calssification_model.py
+++ Individual Files/Rajeev_calssification_model.py
@@ -1,7 +1,4 @@
 #-----------------------------------IMPORT LIBTATIES--------------------------------#
-import pandas as pd
-import numpy as np
-from sklearn import preprocessing
 import matplotlib.pyplot as plt
 
 # Core libraries
@@ -17,7 +14,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 # Sklearn classification model evaluation function
 from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import confusion_matrix
 
 plt.rc("font", size=14)
 from sklearn.linear_model import LogisticRegression
@@ -34,9 +31,7 @@ from sklearn import metrics
 #--------------------------------INPUT FILE-------------------------------------#
 input_path=r"C:\Users\ADMIN\Desktop\masters\GW\intro to data mining"
 df=pd.read_csv(r"%s\cleaned_data5.csv"%input_path)
-#df=pd.read_csv(r"C:\Users\ADMIN\Desktop\masters\GW\intro to data mining\cleaned_data5.csv")
-print(df.head())
-print(df.columns)
+df["Grades"]=df["Grades"].replace([1,2,3,4,5,6,7],["Mostly A's","Mostly B's","Mostly C's","Mostly D's","Mostly F's","None of these grades","Not sure"])
 #--------------------------Grades BY Television-------------------------------#
 x, y ="Television", "Grades"
 
@@ -76,10 +71,31 @@ for p in g.ax.patches:
     g.ax.text(txt_x,txt_y,txt)
     
 g.savefig("%s/Grades_BY_Electronic_Devices.png"%input_path)
+
+#--------------------------Grades BY marijuana_use-------------------------------#
+x, y ="marijuana_use", "Grades"
+
+df1=df.groupby(x)[y].value_counts(normalize=True)
+df1=df1.mul(100)
+df1=df1.rename('percent').reset_index()
+
+g=sns.catplot(x=x, y='percent', hue=y, kind="bar", data=df1)
+g.ax.set_ylim(0,100)
+g.fig.set_size_inches(20,10)
+
+for p in g.ax.patches:
+    txt=str(p.get_height().round(1))+"%"
+    txt_x=p.get_x()
+    txt_y=p.get_height()
+    
+    g.ax.text(txt_x,txt_y,txt)
+    
+g.savefig("%s/Grades_BY_marijuana_use.png"%input_path)
+
 # #--------------------------Grades BY use of technical devices-------------------------------#
 #--------------------Synthetic Minority Oversampling Technique (SMOTE)---------------------#
 
-X=df.drop(columns=['index','year','bmi','Vape_Use','Physical_Activity','Grades','race','sex','weight','marijuana_use','cyber_bullied'])
+X=df.drop(columns=['index','year','bmi','Vape_Use','Physical_Activity','Grades','race','sex','weight','cyber_bullied'])
 #dependent_variable
 y=df[['Grades']]
 
@@ -110,7 +126,7 @@ feature_names=smote_data_X.columns.to_list()
 model=LogisticRegression(random_state=0).fit(df[feature_names].values, df['Grades'].values)
 #Get the score
 score=model.score(df[feature_names].values, df['Grades'].values)
-print("Logistic Regression score is:"score)
+print("Logistic Regression score is:",score)
 
 
 w0=model.intercept_[0]
@@ -135,6 +151,7 @@ y=smote_data_y['Grades']
 
 X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.33, random_state=0)
 
+
 model_dt = DecisionTreeClassifier()
 model_dt.fit(X_train, y_train)
 
@@ -144,28 +161,3 @@ print("DecisionTreeClassifier", accuracy_score(y_train, predictions_dt))
 
 predictions_dt = model_dt.predict(X_test)
 print("DecisionTreeClassifier TEST: ", accuracy_score(y_test, predictions_dt))
-
-
-# we are tuning three hyperparameters right now, we are passing the different values for both parameters
-grid_param = {
-    'criterion': ['gini', 'entropy'],
-    'max_depth' : range(2,32,1),
-    'min_samples_leaf' : range(1,10,1),
-    'min_samples_split': range(2,10,1),
-    'splitter' : ['best', 'random']
-    
-}
-
-grid_search = GridSearchCV(estimator=model_dt,
-                     param_grid=grid_param,
-                     cv=5,
-                    n_jobs =-1)
-
-
-grid_search.fit(X_train,y_train)
-
-best_parameters = grid_search.best_params_
-print(best_parameters)
-
-print(grid_search.best_score_)
-#The score is not effective after hyperparameter tuning(0.52), I'ts gonna take some time to run in your laptop.
